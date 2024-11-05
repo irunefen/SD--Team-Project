4a4f841fb9f978b785bfb1990736de80f09ad867
jaimeetxebarria <jaimeetxebarria@gmail.com>
2024-11-05 23:19:40 +0100

    Initialize spring boot project

---
+++ src/test/java/es/deusto/sd/strava/StravaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.deusto.sd.strava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StravaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
